a679755f63ceee4a33067924868d71ba4e36697b
Kaley Main <kmain@switch.tv>
2019-01-26 14:02:57 +1100

    Add content

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
+import './normalize.css';
+import './skeleton.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="header">Welcome to Kinky Book Club Sydney!</h1>
+        <hr />
+        <div className="container">
+          <div className="entry">
+            <h2>First meeting</h2>
+            <h5>10th of March 2019</h5>
+            <p>Loving Dominant by John and Libby Warren.<br />
+              <a href="https://fetlife.com/events/755120">Fetlife Event</a>
+            </p>
+          </div>
+        </div>
+        
       </div>
     );
   }
